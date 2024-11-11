ccf4c9f86101be1d67cf912905affbbd4d315600
sufyanamir <sufi.canola@gmail.com>
2024-11-11 14:56:46 +0500

    Make routes of new APIs.

--- routes/api.php
+++ routes/api.php
@@ -13,12 +13,15 @@ Route::middleware(['auth:sanctum'])->group(function () {
 
     Route::post('/addFeed', [ApiController::class, 'addFeed']);
     Route::get('/getFeed', [ApiController::class, 'getFeed']);
+    Route::post('/deleteFeed', [ApiController::class, 'deleteFeed']);
+    Route::post('/likeFeed', [ApiController::class, 'likeFeed']);
 
     Route::get('/getAnimals', [ApiController::class, 'getAnimals']);
     Route::get('/getBreed/{id}', [ApiController::class, 'getBreed']);
 
     Route::post('/addPet', [ApiController::class, 'addPet']);
     Route::get('/getPets', [ApiController::class, 'getPets']);
+    Route::post('/deletePet', [ApiController::class, 'deletePet']);
     Route::post('/addPetImages', [ApiController::class, 'addPetImages']);
 
     Route::get('/getPetDetails/{id}/{key?}', [ApiController::class, 'getPetDetails']);
